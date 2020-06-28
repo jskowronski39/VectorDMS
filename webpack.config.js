8518f2143ff469b9cd6a78cb83b00b5c2e8e4c9e
Jakub Skowroński <j.skowronski39@gmail.com>
2020-06-28 12:02:39 +0200

    Autoload entrypoints

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 var Encore = require('@symfony/webpack-encore');
+const glob = require('glob');
+const basename = require('basename');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
+
+const searchPattern = 'assets/**/*.js';
+const paths = glob.sync(searchPattern);
+paths.forEach((path) => {
+    Encore.addEntry(basename(path), `./${path}`);
+});
 
 Encore
     // directory where compiled assets will be stored
@@ -13,19 +21,6 @@ Encore
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
-
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-     */
-    .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -54,7 +49,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
